f1f545e42277344380b38f7616bd618877e633a2
Gabriel Wanderley <gabrielwanderley08@gmail.com>
2024-08-28 13:48:00 -0300

    last

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgmiIwXkPkMc2Q87aYQNxaywpGT4W-sLw",
-  authDomain: "barber-89573.firebaseapp.com",
-  projectId: "barber-89573",
-  storageBucket: "barber-89573.appspot.com",
-  messagingSenderId: "100323102445",
-  appId: "1:100323102445:web:0ae0131184354e079086e4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
